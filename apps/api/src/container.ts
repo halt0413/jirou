6fc9acf2d7b21433a8607e093e7b9b7242c036ae
hsmtDP-T <ittuyo.hashi244@gmail.com>
2026-02-22 23:47:48 +0900

    既存のファイル名とimportをただした

--- apps/api/src/container.ts
+++ apps/api/src/container.ts
@@ -4,7 +4,7 @@ import { drizzle } from "drizzle-orm/d1";
 import * as schema from "./infra/db/schema";
 import { D1PostsRepository } from "./infra/repository/d1Posts.repository";
 import { CreatePostUseCase } from "./application/posts/createPost.usecase";
-import { FindPostsByUserIdUseCase } from "./application/posts/FindPostsByUserIdUseCase";
+import { FindPostsByUserIdUseCase } from "./application/posts/findPostsByUserId.usecase";
 export const createContainer = (env: Env) => {
     const db = drizzle(env.DB, {schema});
     const postRepository = new D1PostsRepository(db);
