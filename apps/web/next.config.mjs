d11547fe75d0dccfe6dc393994996ef76c94f2f1
leon <2240206ecc@gmail.com>
2026-02-28 00:57:56 +0900

    fix: SVGルールのパターンを修正

--- apps/web/next.config.mjs
+++ apps/web/next.config.mjs
@@ -10,7 +10,7 @@ const nextConfig = {
     root: path.resolve(__dirname, "../.."), // monorepo root
 
     rules: {
-      "*svg": {
+      "*.svg": {
         loaders: ["@svgr/webpack"],
         as: "*.js",
       },
